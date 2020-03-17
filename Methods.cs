9727ffd58b2e35a0449306edc37aa84143e04043
BOSS-xu <58324340+BOSS-xu@users.noreply.github.com>
2020-03-17 21:02:56 +0800

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_5
+{
+    class Methods
+    {
+        public static void tro(string str)
+        {
+            int i = 0;
+            String s;
+            int b;
+            char[] ch = new char[str.Length];
+            try
+            {
+
+                foreach (char c in str)
+                {
+                    if (c >= 48 && c <= 58)
+                    {
+                        ch[i] = c;
+                        i++;
+                    }
+                    else
+                    {
+                        s = new String(ch);
+                        b = int.Parse(s);
+                        Console.WriteLine(b);
+                        ch = null;
+                        continue;
+                    }
+                }
+            }
+            catch { }
+        
+        }
+    }
+}
